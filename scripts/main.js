3651f29f130535fa7b193fa638d7ffbe04254659
wooods-marshes <13080352723@163.com>
2022-07-29 09:35:10 +0800

    add timeshow

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,10 @@
 (function (window) {  
     'use strict'
+    var TIMES_SELECTOR = '[data-woods-marshes="showtime"]';
+    var App = window.App;
+    var Times = App.Times;
+
+    var times = new Times(TIMES_SELECTOR);
+    times.showTime();
     
 })(window);
---
+++ scripts/time.js
@@ -0,0 +1,43 @@
+(function (window) {  
+    'use strict';
+    var App = window.App || {};
+    var $ = window.jQuery;
+
+    function Times(selector) {  
+        if (! selector) {
+            return new Error('No selector providede');
+        }
+        this.$element = $(selector);
+        if (this.$element.length === 0) {
+            throw new Error('Could not find element with selector: ' + selector);
+        }
+    }
+
+    Times.prototype.showTime = function () { 
+        //this.$element.html(getTime());
+        //setInterval(getTime(),1000);
+        setInterval(function () {  
+            this.$element.html(getTime());
+        }.bind(this)
+        , 1000);
+    }; 
+
+    var getTime = function () {  
+        var date = new window.Date();
+        var year = date.getFullYear();
+        var month = date.getMonth() + 1;
+        var day = date.getDate();
+        var week = date.getDay();
+        week = "星期" + '日一二三四五六'.charAt(week);
+        var hour = date.getHours();
+        hour = hour < 10 ? '0' + hour: hour;
+        var minute = date.getMinutes();
+        minute = minute < 10 ? '0' + minute: minute;
+        var second = date.getSeconds();
+        second = second < 10 ? '0' + second: second;
+        return 'THE TIME IS' + ' ' + year + '-' + month + '-' + day + ' ' + week + ' ' + hour + ':' + minute + ':' + second;
+    };
+
+    App.Times = Times;
+    window.App = App;
+})(window);
